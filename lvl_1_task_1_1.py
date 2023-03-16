eda350fe36a98ff9851210127b24e9927769a92a
Elena <elena52s@yandex.ru>
2023-03-16 20:16:41 +0300

    Решена задача 1_1

---
+++ lvl_1_task_1_1.py
@@ -0,0 +1,5 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs[0:14])
+print(my_favorite_songs[-13:])
+print(my_favorite_songs[16:30])
+print(my_favorite_songs[-26:-15])
